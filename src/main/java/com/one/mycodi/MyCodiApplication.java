48665ae9c62c74e91868dc96af3b76d12e3ec517
tanibourne <tanibourne@gmail.com>
2022-08-12 16:03:47 +0900

    first init

---
+++ src/main/java/com/one/mycodi/MyCodiApplication.java
@@ -0,0 +1,13 @@
+package com.one.mycodi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCodiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCodiApplication.class, args);
+    }
+
+}
