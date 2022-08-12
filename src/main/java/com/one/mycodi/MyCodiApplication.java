d81231449601cb6e44c3b37733eb0591ed8793e6
KORJaeyoungKim <jy0511_@naver.com>
2022-08-12 16:27:01 +0900

    [Jaeyoung] <chore> test pull request

--- src/main/java/com/one/mycodi/MyCodiApplication.java
+++ src/main/java/com/one/mycodi/MyCodiApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyCodiApplication {
 
+
+//test - 추후 삭제 요망
+
+
     public static void main(String[] args) {
         SpringApplication.run(MyCodiApplication.class, args);
     }
