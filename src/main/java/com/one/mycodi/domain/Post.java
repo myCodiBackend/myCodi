8ce8e0d8cc9cfb0ca983f602f02b50bb377a6890
tanibourne <98215154+tanibourne@users.noreply.github.com>
2022-08-13 22:44:26 +0900

    Upate Post.java

---
+++ src/main/java/com/one/mycodi/domain/Post.java
@@ -0,0 +1,50 @@
+package com.one.mycodi.domain;
+
+import com.one.mycodi.dto.request.PostRequestDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column
+    private String imgUrl;
+
+    @Column(nullable = false)
+    private String content;
+
+
+    @JoinColumn(name = "member_id", nullable = false)
+    @ManyToOne
+    private Member member;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "post")
+    private List<Comment> comments = new ArrayList<>();
+
+
+//    int heart;
+
+
+    public void update(PostRequestDto postRequestDto) {
+        this.title = postRequestDto.getTitle();
+        this.content = postRequestDto.getContent();
+    }
+
+
+}
